faeb2790012f2633adee24b75120e77b7cd6b453
LukeMascarinas <lukegwapogwapo@gmail.com>
2026-07-17 18:58:01 +0800

    Added first Java program

---
+++ main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Mascariñas, Luke Aidan B.";
+        String course = "BSIT";
+        String school = "Interface Computer College";
+        String place = "Brgy. Alegre, Oton, Iloilo";
+
+        System.out.println("Hello, World!");
+        System.out.println("Name: " + name);
+        System.out.println("Course: " + course);
+        System.out.println("School: " + school);
+        System.out.println("Place: " + place);
+    }
+}
